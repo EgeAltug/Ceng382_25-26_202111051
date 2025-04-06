270b999350c6aecee20c4702cfd6307c73a79cf5
AhmetOzturk12 <altug.ege4@gmail.com>
2025-04-06 22:45:50 +0300

    Removed unnecessary files

--- week5Project/obj/Debug/net8.0/week5Project.AssemblyInfo.cs
+++ week5Project/obj/Debug/net8.0/week5Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("week5Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9b2546a173d73765a04b4e3ccfabf63281227ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d599260ba8c66d9fbdf222a1969d5b437ab5e628")]
 [assembly: System.Reflection.AssemblyProductAttribute("week5Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("week5Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
