c9f2547d87193affb2b3401ac843dee3772960da
AhmetOzturk12 <altug.ege4@gmail.com>
2025-04-09 02:42:12 +0300

    Testing

--- week7Project/obj/Debug/net8.0/week7Project.AssemblyInfo.cs
+++ week7Project/obj/Debug/net8.0/week7Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("week7Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9f982dbf53c47571a0afb179c21a4c0d9f94573")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9a6b76d6a8324a4147ddee199067f2accb8a48a")]
 [assembly: System.Reflection.AssemblyProductAttribute("week7Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("week7Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
