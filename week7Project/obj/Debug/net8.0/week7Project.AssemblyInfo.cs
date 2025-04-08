a9f982dbf53c47571a0afb179c21a4c0d9f94573
AhmetOzturk12 <altug.ege4@gmail.com>
2025-04-09 02:39:45 +0300

    Changes how Index.cshtml works

--- week7Project/obj/Debug/net8.0/week7Project.AssemblyInfo.cs
+++ week7Project/obj/Debug/net8.0/week7Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("week7Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9927856b1daf12acc1dd185cde7063700c41a532")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6abdac8b10a9b2e64bdeb960406cedfd5f3577e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("week7Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("week7Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
